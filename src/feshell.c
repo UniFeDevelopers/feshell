7ecd16cf98ad1d1baf47a2a13ae6cdeb99f2fc0a
nzambello <nicola.zambello@student.unife.it>
2017-05-04 22:53:35 +0200

    fix errori input e loop, gestione ctrl-d, exit e clear

--- src/feshell.c
+++ src/feshell.c
@@ -2,61 +2,71 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <signal.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
     int pid, status;
-    char buff[255];
+    char buff[1024];
     char *cmd;
-    char **args;
+    char **args = NULL;
     int n_args;
-    int i;
 
-    while (1) {
-        printf("> ");
+    printf("> ");
+    while (fgets(buff, 1025, stdin) != NULL) {
         args = NULL;
         n_args = 0;
 
-        fgets(buff, 256, stdin);
-        cmd = strtok(buff, " \n\t;");
+        if (!strcmp(buff, "\n")) {
+            printf("> ");
+            continue;
+        }
+        else if (!strcmp(buff, "clear")) {
+            system("clear");
+            continue;
+        }
+
+        cmd = strtok(buff, " \t\n;");
 
         if (strstr(cmd, "exit") != NULL) exit(0);
 
         while (cmd != NULL) {
             args = realloc(args, sizeof (char*) * ++n_args);
 
-            if (args == NULL) exit (-1);
+            if (args == NULL) exit (1);
 
             if (strlen(cmd)) {
                 args[n_args - 1] = cmd;
             }
 
-            cmd = strtok(NULL, " \n\t;");
+            cmd = strtok(NULL, " \t\n;");
         }
 
-        printf("\nLISTA COMANDI (%d)\n", n_args);
-        for (i = 0; i < n_args; i++) {
-            printf("%s - ", args[i]);
-        }
-        printf("\n");
+        if (n_args) {
+            pid = fork();
 
-        // realloc one extra element for the last NULL  (?)
-        args = realloc(args, sizeof (char*) * (n_args + 1));
-        args[n_args] = 0;
-
-        pid = fork();
-
-        if (pid == 0) {
-            if (execvp(args[0], args) == -1) {
-                fprintf(stderr, "Exec fallita: %s", args[0]);
+            if (pid == 0) {
+                if (execvp(args[0], args) == -1) {
+                    fprintf(stderr, "-feshell: %s: ", args[0]);
+                    perror("");
+                }
+            } else if (pid > 0) {
+                pid = wait(&status);
+                /*
+                 * gestione dello stato
+                 */
+            } else {
+                printf("fork fallita");
             }
-            exit(1);
-        } else if (pid > 0) {
-            pid = wait(&status);
-            //<gestione dello stato>
-        } else {
-            printf("fork fallita");
         }
+
+        printf("> ");
+        fflush(stdin);
+    }
+
+    if (feof(stdin)) {
+        printf("exit");
+        kill(getppid(), SIGINT);
     }
 
     free(args);
