0d7705a08382060c047f5160e14456fe901ee068
nzambello <nicola.zambello@student.unife.it>
2017-05-13 13:50:07 +0200

    parsing funzionante, da testare nella shell

--- src/parse_lib/parse_lib.c
+++ src/parse_lib/parse_lib.c
@@ -6,16 +6,6 @@
 #include "../feshell_lib.h"
 #include "./parse_lib.h"
 
-void appendCoda(cmd_t **cmd_list, cmd_t *el) {
-    cmd_t *head;
-
-    head = *cmd_list;
-    while (head->next != NULL) {
-        head = head->next;
-    }
-    head->next = el;
-}
-
 int countTokens(char *str, char *limit) {
     int count = 0;
     char *token;
@@ -30,6 +20,16 @@ int countTokens(char *str, char *limit) {
     }
 
     return count;
+}
+
+void appendCoda(cmd_t **cmd_list, cmd_t *el) {
+    cmd_t *head;
+
+    head = *cmd_list;
+    while (head->next != NULL) {
+        head = head->next;
+    }
+    head->next = el;
 }
 
 void appendTesta(cmd_t **cmd_list, cmd_t *el) {
@@ -52,25 +52,55 @@ void appendElement(cmd_t **cmd_list, cmd_t *el, int mode) {
 }
 
 int parse(char *buff) {
-    char buff_copy[MAX_DIM_BUFF];
+    cmd_t *cmd_list, *tmp;
+    cmd_list = NULL;
 
-    strcpy(buff_copy, buff);
+    tokenize_n_push(buff, &cmd_list);
 
-    // 1- separa per <, >, >>, |
-    // 2- manda a tokenize_n_push
-    // + crea lista e passala a t_n_p
+    if (cmd_list == NULL) return 1;
 
+    tmp = cmd_list;
+    char **exec_args;
+    int status;
+    int i;
+
+    while (tmp != NULL) {
+        exec_args = (char **) malloc(sizeof(char *) * (tmp->n_args + 1));
+        for (i = 0; i < tmp->n_args; i++) {
+            exec_args[i] = (char *) malloc(sizeof(char *) * strlen(tmp->args[i]));
+            strcpy(exec_args[i], tmp->args[i]);
+        }
+        exec_args[i] = NULL;
+
+        if (fork() == 0) {
+            if (execvp(exec_args[0], exec_args) == -1) {
+                fprintf(stderr, "-feshell: %s: ", exec_args[0]);
+                perror("");
+                exit(1);
+            }
+            exit(0);
+        }
+        else {
+            wait(&status);
+        }
+        tmp = tmp->next;
+    }
 
     return 0;
 }
 
-void tokenize_n_push(char *buff, /*LISTA*/) {
+void tokenize_n_push(char *buff, cmd_t **lista) {
     char buff_copy[MAX_DIM_BUFF];
     char *cmd = NULL;
     char **args = NULL;
     int n_args = 0;
     int tokens;
-    int i;
+    int i, j;
+
+    cmd_t *new;
+
+    int is_file_out = 0;
+    int is_file_in = 0;
 
     strcpy(buff_copy, buff);
     tokens = countTokens(buff_copy, " \t\n;");
@@ -81,38 +111,74 @@ void tokenize_n_push(char *buff, /*LISTA*/) {
     cmd = strtok(buff, " \t\n;");
     for (i = 0; i < tokens; i++) {
         if (strlen(cmd) && strcmp(cmd, "\t") && strcmp(cmd, " ")) {
-            args[i] = (char *) malloc(sizeof(char) * (strlen(cmd) + 1));
-            strcpy(args[i], cmd);
-            n_args++;
-
-            if (!strcmp(args[i], "|") || !strcmp(args[i], ">>") || !strcmp(args[i], ">")) {
+            if (!strcmp(cmd, "|")) {
                 // crea struct
 
-                args = NULL;
+                new = (cmd_t *) malloc(sizeof(cmd_t));
+
+                new->node_type = 1;
+                strcpy(new->nome, args[0]);
+                for (j = 0; j < n_args; j++) {
+                    new->args[j] = (char *) malloc(sizeof(char) * strlen(args[j]));
+                    strcpy(new->args[j], args[j]);
+                }
+                new->n_args = n_args;
+                new->next = NULL;
+
+                appendElement(lista, new, 1);
+
+                args = (char **) malloc(sizeof(char *) * (tokens + 1));
                 n_args = 0;
             }
-            else if (!strcmp(args[i], "<")) {
-                // crea struct
+            else if (!strcmp(cmd, ">>") || !strcmp(cmd, ">")) {
+                // cose sul file
+                is_file_out = 1;        // distingui
 
-                args = NULL;
+                //appendElement(lista, new, 1);
+            }
+            else if (!strcmp(cmd, "<")) {
+                // crea struct
+                // cose sul file successivo
+                is_file_in = 1;
+
+                //appendElement(lista, new, 0);
+
+                args = (char **) malloc(sizeof(char *) * (tokens + 1));
                 n_args = 0;
+            }
+            else {
+                if (is_file_in) {
+                    args[0] = "cat";
+                    args[1] = (char *) malloc(sizeof(char) * (STRING_MAX_L + 1));
+                    strcpy(args[1], cmd);
+                }
+                else {
+                    args[n_args] = (char *) malloc(sizeof(char) * (STRING_MAX_L + 1));
+                    strcpy(args[n_args], cmd);
+                    n_args++;
+                }
+
+                is_file_in = 0;
+                is_file_out = 0;
             }
         }
 
         cmd = strtok(NULL, " \t\n;");
     }
 
-    args[i] = NULL;
+    new = (cmd_t *) malloc(sizeof(cmd_t));
 
-    if (n_args) {
-        /*
-        if (execute(n_args, args) == 1) {
-            exit(1);
-        }
-        */
-
-        // 3- crea struct e lista, pusha sulla lista
+    new->node_type = 1;
+    strcpy(new->nome, args[0]);
+    for (j = 0; j < n_args; j++) {
+        new->args[j] = (char *) malloc(sizeof(char) * strlen(args[j]));
+        strcpy(new->args[j], args[j]);
     }
+    new->n_args = n_args;
+    new->next = NULL;
+    args[j] = NULL;
+
+    appendElement(lista, new, 1);
 
     free(args);
     free(cmd);
--- src/parse_lib/parse_lib.h
+++ src/parse_lib/parse_lib.h
@@ -4,16 +4,17 @@
 #define STRING_MAX_L 1024
 
 typedef struct _cmd_t {
-    int node_type;              // 0 = exec, 1 = file
-    char nome[STRING_MAX_L];
-    char *args[STRING_MAX_L];
+	int node_type;              // 0 = exec, 1 = file
+	char nome[STRING_MAX_L];
+	char *args[STRING_MAX_L];
+	int n_args;
 
-    struct _cmd_t *next;
+	struct _cmd_t *next;
 } cmd_t;
 
 // 0 per < , 1 per tutti gli altri casi
 void appendElement(cmd_t **, cmd_t *, int);
 int parse(char *);
-void tokenize_n_push(char *);
+void tokenize_n_push(char *, cmd_t **);
 
 #endif
