dcc953104d339dc8fdfe4f79d9faffbb898af9a4
nzambello <nicola.zambello@student.unife.it>
2017-06-02 17:00:20 +0200

    nuovi elementi per la struttura dei comandi per implementare le redirect

--- src/feshell.test.c
+++ src/feshell.test.c
@@ -34,9 +34,36 @@ int main(void) {
         printf("\nLISTA:\n\n");
 
         while (tmp != NULL) {
-            printf("- el->node_type:\t%i\n", tmp->node_type);
+            printf("- el->node_type:\t");
+            switch (tmp->node_type) {
+                case 0:
+                    printf("%d -> exec\n", tmp->node_type);
+                    break;
+                case 1:
+                    printf("%d -> read\n", tmp->node_type);
+                    break;
+                case 2:
+                    printf("%d -> write\n", tmp->node_type);
+                    break;
+                case 3:
+                    printf("%d -> read write\n", tmp->node_type);
+                    break;
+                case 4:
+                    printf("%d -> append\n", tmp->node_type);
+                    break;
+                case 5:
+                    printf("%d -> read append\n", tmp->node_type);
+                    break;
+                default:
+                    fprintf(stderr, "ERRORE NODE TYPE: %d\n", tmp->node_type);
+                    break;
+            }
+
             printf("- el->nome:     \t%s\n", tmp->nome);
             printf("- el->n_childs: \t%d\n", tmp->n_childs);
+            printf("- el->fileIn:   \t%s\n", tmp->fileIn);
+            printf("- el->fileOut:  \t%s\n", tmp->fileOut);
+
             printf("- el->args (%d):\n", tmp->n_args);
             for (i = 0; i < tmp->n_args; i++) {
                 printf("           \t%s\n", tmp->args[i]);
--- src/parse_lib/parse_lib.h
+++ src/parse_lib/parse_lib.h
@@ -5,20 +5,29 @@
 #define TESTA 0
 #define CODA 1
 
+/*
+ * NODE TYPES *
+ *
+ * 0:    exec
+ * 1:    read
+ * 2:    write
+ * 3:    read write
+ * 4:    append
+ * 5:    read append
+ */
+
 typedef struct _cmd_t {
-    int node_type;              // 0 = exec, 1 = file write, 2 = file append
+    int node_type;
     char nome[STRING_MAX_L];
     char *args[STRING_MAX_L];
+    char fileOut[STRING_MAX_L];
+    char fileIn[STRING_MAX_L];
     int n_args;
     int n_childs;
 
     struct _cmd_t *next;
 } cmd_t;
 
-// 0 per < , 1 per tutti gli altri casi
-void appendElement(cmd_t **, cmd_t *, int);
 cmd_t *parse(char *);
-int countTokens(char *, char *);
-void tokenize_n_push(char *, cmd_t **);
 
 #endif
